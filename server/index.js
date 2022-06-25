acc39ff5e8c13e1cb886904f52ebf9b6b7332933
SamRushan <nirushanan.srimurugan@gmx.de>
2022-06-25 21:29:16 +0200

    index

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-
+const fs = require('fs');
+const { request } = require("http");
+const { response } = require("express");
 const app = express();
 const port = 3000;
 
+
+const path = './server/data.json'
+
+fs.readFile('./server/data.json', (err, data) => {
+    if (err) throw err;
+    console.log(JSON.parse(data));
+});
+
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+app.get("/module", (req, res) => {
+    let test 
+
+    fs.readFile('./server/data.json', (err, data) => {
+        if (err) throw err;
+        test = JSON.parse(data);
+    });
+    
+    res.json({test})
 });
+
+app.get("/modulegruppe", (req, res) => {
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
